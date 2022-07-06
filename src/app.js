701e4d7f5fd3986104c87628a53e53c1dcbc199c
ISFlores <isaacfloresv@gmail.com>
2022-07-06 21:35:48 +0000

    Inicio de maquetado

--- src/app.js
+++ src/app.js
@@ -4,6 +4,30 @@ import "./style.css";
 
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
+function random() {
+  let card = math.floor(math.random() * 14);
+  let num = 0;
+  if (card >= 11) {
+    switch (card) {
+      case 11:
+        return (num = J);
+        break;
+      case 12:
+        return (num = Q);
+        break;
+      case 13:
+        return (num = K);
+        break;
+      case 14:
+        return (num = A);
+        break;
+    }
+  } else return num;
+}
+function generadorcard() {
+  let card = null;
+  return card;
+}
 
 window.onload = function() {
   //write your code here
